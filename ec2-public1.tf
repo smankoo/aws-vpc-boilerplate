bf1f8ee6208016e242d3494f1d93d8ad9c4eee52
Sumeet Singh Mankoo <sumeet.mankoo@morningstar.com>
2020-10-06 15:56:30 -0400

    added user_data to web server

--- ec2-public1.tf
+++ ec2-public1.tf
@@ -8,8 +8,20 @@ resource "aws_instance" "web1" {
   vpc_security_group_ids = [aws_security_group.allow_my_ssh.id, aws_security_group.web.id]
   iam_instance_profile = aws_iam_instance_profile.websource_read.name
 
+  user_data = <<EOF
+#! /bin/bash
+sudo apt-get update
+sudo apt-get install -y apache2
+sudo systemctl start apache2
+sudo systemctl enable apache2
+echo "<h1>Deployed via Terraform</h1>" | sudo tee /var/www/html/index.html
+EOF
   
   tags = {
     Name = "web1"
   }
+}
+
+output "instance_public_ip_addr" {
+  value = aws_instance.web1.public_ip
 }
